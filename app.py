f4ed2094665722044f4e9a6933266401e06ee058
joel bispo <joelbispo04@hotmail.com>
2023-01-31 10:50:51 -0300

    segunda rota criada

--- app.py
+++ app.py
@@ -9,8 +9,13 @@ app = Flask(__name__)
 def homepage():
     return "Iniciando a criação do site"
 
+@app.route("/contatos")
+def contatos():
+    return "E-mail: joelbispo04@hotmail.com"
+
 
 
 
 # Colocando o site no ar
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
